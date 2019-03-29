d9c076c68695efb3f81c08706b062e7ab5e5c895
Cadmus-design <cadmus.sung@gmail.com>
2019-03-30 00:18:17 +0800

    作品集
    gtag manager 安裝

--- portfolio/assets/js/all.js
+++ portfolio/assets/js/all.js
@@ -12,3 +12,11 @@ $( window ).resize(function() {
 	  gtag('js', new Date());
 
 	  gtag('config', 'UA-6789868-5');
+
+/*gtag manager*/
+(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-KTR3PGW');
+/*gtag manager*/
