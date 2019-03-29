3552ddc8bd621e8cca0e824e75ca38a9669278b4
Cadmus-design <cadmus.sung@gmail.com>
2019-03-29 12:05:15 +0800

    作品集

---
+++ portfolio/assets/js/all5.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+
+$( window ).resize(function() {
+  $( "body" ).prepend( "<div>" + $( window ).width() + "</div>" );
+
+});
+
+
+ $( ".test" ).html("<small>test</small>");
+
+
+
+
+});
+
+
