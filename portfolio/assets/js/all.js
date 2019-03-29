aff62646d49b3326f18406a6ee01b4a49eea43a8
Cadmus-design <cadmus.sung@gmail.com>
2019-03-30 00:04:15 +0800

    作品集
    完成部分測試

---
+++ portfolio/assets/js/all.js
@@ -0,0 +1,14 @@
+
+
+
+
+$( window ).resize(function() {
+  $( "body" ).prepend( "<div>" + $( window ).width() + "</div>" );
+
+});
+
+ window.dataLayer = window.dataLayer || [];
+	  function gtag(){dataLayer.push(arguments);}
+	  gtag('js', new Date());
+
+	  gtag('config', 'UA-6789868-5');
