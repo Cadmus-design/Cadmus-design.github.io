bdfe413be233544ac920f67b8c5c7309fe42dea0
Cadmus-design <cadmus.sung@gmail.com>
2019-07-23 00:28:05 +0800

    jQuery
    ch2-4

--- WEBMaster/jQuery/Chapter02/all/js/main.js
+++ WEBMaster/jQuery/Chapter02/all/js/main.js
@@ -159,6 +159,7 @@ $(function(){
 	});
 });
 */
+/*
 $(function(){
 	$('#typo')
 	.on('mouseover',function(){
@@ -172,5 +173,64 @@ $(function(){
 			color:'',
 			background:''
 		});
+	});
+});
+*/
+//ch2-4
+/*
+$(function(){
+	$('#typo .inner').on('click', function(){
+		$('#typo .inner').animate({
+			opacity:'0',
+			fontSize:'0px'
+		},
+		1500
+		);
+	});
+});
+*/
+/*
+$(function(){
+	$('#typo .inner').on('click' ,function(){
+		$('#typo .inner').animate({
+			color:'#000'
+		},
+		1500
+		);
+	});
+});
+*/
+/*
+$(function(){
+	$('#typo .inner')
+	.css('top','-100px')
+	.on('click', function(){
+		$('#typo .inner').animate({
+			top: '100px'
+		},
+		1500,
+		'easeInOutBounce',
+		function(){
+			$('#typo .inner').animate({top:'0px'},500);
+		}
+		);
+	});
+});
+*/
+$(function(){
+	$('#typo')
+	.on('mouseover',function(){
+		$('#typo').stop(true).animate({
+			backgroundColor:'#ffff00'
+		},
+		500
+		);
+	})
+	.on('mouseout', function(){
+		$('#typo').stop(true).animate({
+			backgroundColor:'#ddd'
+		},
+		500
+		);
 	});
 });
