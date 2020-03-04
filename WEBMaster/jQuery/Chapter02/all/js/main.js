b2aee2851e3da0ba38c4f17670c7e848d33a152a
Cadmus-design <cadmus.sung@gmail.com>
2020-03-04 22:12:19 +0800

    Hamipoint
    1.活動標題調整為：全站2倍贈點賺翻天
    2.於流程圖上方插入標題：簡單三步驟   Hami Point兩倍送
    3.外連按鈕移至熱門店鋪下方，並調整文案
    a. (字級放大/原前往專屬連結，開始賺點)超過60間購物網站，立即行動！
    b.Button左(原Hami Point購物賺點網站)：前往購物賺點網站，看更多
    c.Button右(原下載Hami Pay APP)：下載Hami Pay，賺點更便利
    4.移除按鈕”超過60間購物網站+”
    5.活動注意事項的標題字級縮小至與上方按鈕字級等大
    6.注意事項下方內文，原活動日期2020/3/2改為2020/3/9
    7. (桌機版) 流程圖的分隔線需置中於各流程圖中間(目前皆偏右，示意圖請參照附檔PPT)
    8. (手機版) 活動內容左右各縮排0.5，保留呼吸空間

--- WEBMaster/jQuery/Chapter02/all/js/main.js
+++ WEBMaster/jQuery/Chapter02/all/js/main.js
@@ -217,6 +217,7 @@ $(function(){
 	});
 });
 */
+/*
 $(function(){
 	$('#typo')
 	.on('mouseover',function(){
@@ -229,6 +230,135 @@ $(function(){
 	.on('mouseout', function(){
 		$('#typo').stop(true).animate({
 			backgroundColor:'#ddd'
+		},
+		500
+		);
+	});
+});
+*/
+
+/*20200302*/
+//   CH1
+/*
+$(function(){
+	$('#typo').css('color','#ebc000');
+});
+*/
+
+/*
+$(function(){
+	$('#typo .inner').css('transform','rotate(10deg)');
+});
+*/
+//    CH2
+/*
+$(function(){
+	$('.page-main > div:nth-child(1) .inner').css('opacity','0.5');
+});
+
+$(function(){
+	$('#typo').css(
+	{
+		'fontSize':'50px',
+		'backgroundColor':'#300aaa',
+		'color':'#ddd'
+	})
+});
+*/
+//    CH3
+/*
+$(function(){
+	$('#typo').on('mouseover', function(){
+		$('#typo').css('color','#ebc000');
+	});
+});
+*/
+/*
+$(function(){
+	$('#typo').on('click', function(){
+		$('#typo').css('backgroundColor','#ae5e9b');
+		$('header').css('color','#ae5e9b');
+	});
+});
+*/
+/*
+$(function(){
+	$('#typo').on('mouseover', function(){
+		$('#typo').css({
+			color: '#ebc000',
+			backgroundColor: '#ae5e9b'
+		});
+	});
+	$('#typo').on('mouseout',function(){
+		$('#typo').css({
+			color:'',
+			backgroundColor:''
+		});
+	});
+});
+*/
+/*
+$(function(){
+	$('#typo')
+	.on('mouseover',function(){
+		$('#typo').css({
+			color: '#ebc000',
+			backgroundColor: '#ae5e9b'
+		});
+
+	})
+	.on('mouseout', function(){
+		$('#typo').css({
+			color:'',
+			backgroundColor: ''
+		});
+	})
+})
+*/
+//   CH2 step3
+/*
+$(function(){
+	$('#typo .inner').on('click', function(){
+		$('#typo .inner').animate({
+			//opacity: 0,
+			//fontSize: '0px'
+			//color:'#ebc000'
+		},
+		1500
+		);
+	});
+});
+*/
+/*
+$(function(){
+	$('#typo .inner')
+	.css('top', '-100px')
+	.on('click', function(){
+		$('#typo .inner').animate({
+			top: '100px'
+		},
+		1500,
+		'swing',
+		//加入動畫結束的處理程序
+		function(){
+			$('#typo .inner').animate({top: '0px'},500,'linear');
+		}
+		);
+	});
+});
+*/
+$(function(){
+	$('#typo')
+	.on('mouseover',function(){
+		$('#typo').stop(true).animate({
+			backgroundColor: '#ae5e9b'
+		},
+		500
+		);
+	})
+	.on('mouseout', function(){
+		$('#typo').stop(true).animate({
+			backgroundColor: '#3498db'
 		},
 		500
 		);
