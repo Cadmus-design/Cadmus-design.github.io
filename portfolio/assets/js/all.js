5838700305cf0fc2e2fe2b8d3af53d7be85126db
Cadmus-design <cadmus.sung@gmail.com>
2020-05-01 12:47:16 +0800

    作品集
    更新GTM

--- portfolio/assets/js/all.js
+++ portfolio/assets/js/all.js
@@ -21,3 +21,10 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
 })(window,document,'script','dataLayer','GTM-KTR3PGW');
 /*gtag manager*/
+/* Google Tag Manager (noscript)*/
+$(document).ready(function() {
+    $(document.body).prepend(
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KTR3PGW"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+);
+});/* Google Tag Manager (noscript)*/
