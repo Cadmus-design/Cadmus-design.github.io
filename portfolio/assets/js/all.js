82ef9d59cf2386bba55ff0d764fded542a1225e9
Cadmus-design <cadmus.sung@gmail.com>
2020-05-20 21:19:55 +0800

    國際分公司
    8個分公司，2種語言(日本三種語言)
    共85個頁面 meta title / meta description / keywords 調整與更新

--- portfolio/assets/js/all.js
+++ portfolio/assets/js/all.js
@@ -23,8 +23,5 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 /*gtag manager*/
 /* Google Tag Manager (noscript)*/
 $(document).ready(function() {
-    $(document.body).prepend(
-<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KTR3PGW"
-height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-);
+    $(document.body).prepend('<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KTR3PGW"height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>');
 });/* Google Tag Manager (noscript)*/
