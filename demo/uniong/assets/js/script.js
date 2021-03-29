0c644eb445297b50015ba0c2e97f7eecea8d9a97
Cadmus-design <cadmus.sung@gmail.com>
2021-03-29 10:20:44 +0800

    刪除客服網站
    測試結束

--- demo/uniong/assets/js/script.js
+++
@@ -1,9 +0,0 @@
-
-$(document).ready(function(){
-	$('.pic a').lightBox({
-		imageLoading: 'images/lightbox/loading.gif',
-		imageBtnClose: 'images/lightbox/close.png',
-		imageBtnPrev: 'images/lightbox/prev.png',
-		imageBtnNext: 'images/lightbox/next.png'
-	});
-});
