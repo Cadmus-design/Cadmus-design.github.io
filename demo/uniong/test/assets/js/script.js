8d76c533c057596e697c2c3140c9f8b3bfde13de
Cadmus-design <cadmus.sung@gmail.com>
2021-03-31 16:46:13 +0800

    凱發小幫手DEMO

---
+++ demo/uniong/test/assets/js/script.js
@@ -0,0 +1,9 @@
+
+$(document).ready(function(){
+	$('.pic a').lightBox({
+		imageLoading: 'images/lightbox/loading.gif',
+		imageBtnClose: 'images/lightbox/close.png',
+		imageBtnPrev: 'images/lightbox/prev.png',
+		imageBtnNext: 'images/lightbox/next.png'
+	});
+});
