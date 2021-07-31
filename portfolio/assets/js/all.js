c984b8937990e1cf39d3d8c0efc8f6b7bca88d5d
Cadmus-design <cadmus.sung@gmail.com>
2021-08-01 00:53:19 +0800

    作品集更新

--- portfolio/assets/js/all.js
+++ portfolio/assets/js/all.js
@@ -32,3 +32,60 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
 })(window,document,'script','dataLayer','GTM-TZGCSJH');
 /* End Google Tag Manager 2021-07-30*/
+
+$(function(){
+        var _header=$('.navbar').offset().top;
+        var onScroll = function(e){
+            var _wrapTop=$('.fbox').offset().top;
+            if(_wrapTop > _header) $('.navbar').addClass('on');
+            else $('.navbar').removeClass('on');
+        }
+        var onHashChange = function(e){
+            if(e){e.preventDefault();}
+            var $this = $(this);
+            var target = window.location.hash.slice(1);
+            var $target = $('[scroll-id="' + target + '"]');
+            var differ = $('.navbar')[0].clientHeight;
+            var offsetTop = $target.offset().top;
+            var kvTop = $('#KV').clientHeight;
+            var scrollTop = $(window).scrollTop();
+            var fixed = $('.navbar').hasClass('on');
+            var top =  fixed ? offsetTop - differ : offsetTop - differ *2;
+            $('html, body').stop().animate({scrollTop: top}, 600);
+        }
+        $(window).bind('hashchange', onHashChange);
+        $(window).bind('scroll', onScroll);
+
+        setTimeout(function(e){
+            onHashChange();
+        },1000)
+    });
+   $(function () {
+            $('[slide-target]').click(function () {
+                var target = $(this).attr('slide-target');
+                var $target = $(target);
+                $target.stop().slideToggle(600)
+            })
+
+            function hashchange(f) {
+                var hash = window.location.hash.substring(window.location.hash.indexOf('#') + 1);
+                var $target = $('.' + hash);
+                var top = $target.offset().top;
+                $("html, body").stop().animate({
+                    scrollTop: top
+                }, 600);
+            }
+            
+        });
+   
+    // function checkOffset() {
+    // if($('#social-float').offset().top + $('#social-float').height() 
+    //                                        >= $('#footer').offset().top - 10)
+    //     $('#social-float').css('position', 'absolute');
+    // if($(document).scrollTop() + window.innerHeight < $('#footer').offset().top)
+    //     $('#social-float').css('position', 'fixed'); // restore when you scroll up
+    // /*$('#social-float').text($(document).scrollTop() + window.innerHeight);*/
+    // }
+    // $(document).scroll(function() {
+    //     checkOffset();
+    // });
