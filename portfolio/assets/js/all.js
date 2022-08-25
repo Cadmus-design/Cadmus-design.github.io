0d07baada001352778d226ec339477ed04ac0d2a
Cadmus-design <cadmus.sung@gmail.com>
2022-08-25 11:37:54 +0800

    Helpscout test

--- portfolio/assets/js/all.js
+++ portfolio/assets/js/all.js
@@ -91,3 +91,8 @@ $(function(){
 !function(e,t,n){function a(){var e=t.getElementsByTagName("script")[0],n=t.createElement("script");n.type="text/javascript",n.async=!0,n.src="https://beacon-v2.helpscout.net",e.parentNode.insertBefore(n,e)}if(e.Beacon=n=function(t,n,a){e.Beacon.readyQueue.push({method:t,options:n,data:a})},n.readyQueue=[],"complete"===t.readyState)return a();e.attachEvent?e.attachEvent("onload",a):e.addEventListener("load",a,!1)}(window,document,window.Beacon||function(){});
 window.Beacon('init', '8a690515-147b-4312-a68f-fda45f2b245b')
 /* helpscout end */
+/* helpscout parameter */
+Beacon('show-message', '31dbf5ec-2a14-4b84-a00f-2d1252d85f82', { force: true })
+Beacon('show-message', 'Y8a690515-147b-4312-a68f-fda45f2b245bE', { delay: 1000 })
+
+/* helpscout parameter */
