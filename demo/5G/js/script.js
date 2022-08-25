8834e53d152bfcef501af86443260a2a68bbc47b
Cadmus-design <cadmus.sung@gmail.com>
2022-08-25 10:02:34 +0800

    Helpscout embeded

---
+++ demo/5G/js/script.js
@@ -0,0 +1,4 @@
+/* helpscout start */
+!function(e,t,n){function a(){var e=t.getElementsByTagName("script")[0],n=t.createElement("script");n.type="text/javascript",n.async=!0,n.src="https://beacon-v2.helpscout.net",e.parentNode.insertBefore(n,e)}if(e.Beacon=n=function(t,n,a){e.Beacon.readyQueue.push({method:t,options:n,data:a})},n.readyQueue=[],"complete"===t.readyState)return a();e.attachEvent?e.attachEvent("onload",a):e.addEventListener("load",a,!1)}(window,document,window.Beacon||function(){});
+window.Beacon('init', '8a690515-147b-4312-a68f-fda45f2b245b')
+/* helpscout end */
