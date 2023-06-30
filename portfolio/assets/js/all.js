3f80dafe51f5a17b9bea9e9fe4549031d7fc1efc
Cadmus-design <cadmus.sung@gmail.com>
2023-06-30 10:42:27 +0800

    delete GA4

--- portfolio/assets/js/all.js
+++ portfolio/assets/js/all.js
@@ -4,31 +4,8 @@ $( window ).resize(function() {
 
 });
 */
-/*
- window.dataLayer = window.dataLayer || [];
-	  function gtag(){dataLayer.push(arguments);}
-	  gtag('js', new Date());
-
-	  gtag('config', 'UA-6789868-5');
-*/
-
-/* Google Tag Manager */
-/*
-(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-KTR3PGW');
-*/
-/* End Google Tag Manager */
 
 /*Google Tag Manager 2021-07-30*/
-
-(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-TZGCSJH');
 
 /* End Google Tag Manager 2021-07-30*/
 
@@ -88,14 +65,3 @@ $(function(){
     // $(document).scroll(function() {
     //     checkOffset();
     // });
-
-/* helpscout start */
-!function(e,t,n){function a(){var e=t.getElementsByTagName("script")[0],n=t.createElement("script");n.type="text/javascript",n.async=!0,n.src="https://beacon-v2.helpscout.net",e.parentNode.insertBefore(n,e)}if(e.Beacon=n=function(t,n,a){e.Beacon.readyQueue.push({method:t,options:n,data:a})},n.readyQueue=[],"complete"===t.readyState)return a();e.attachEvent?e.attachEvent("onload",a):e.addEventListener("load",a,!1)}(window,document,window.Beacon||function(){});
-window.Beacon('init', '8a690515-147b-4312-a68f-fda45f2b245b')
-/* helpscout end */
-/* helpscout parameter */
-Beacon('show-message', '31dbf5ec-2a14-4b84-a00f-2d1252d85f82', { force: true })
-Beacon('show-message', 'Y8a690515-147b-4312-a68f-fda45f2b245bE', { delay: 1000 })
-messagingEnabled{false};
-  
-/* helpscout parameter */
